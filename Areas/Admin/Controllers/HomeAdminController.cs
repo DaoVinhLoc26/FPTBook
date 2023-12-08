3768c4f15952ae357fbf298dd9fe2544572bb5b2
tran ky hung <trankyhung225@gmail.com>
2023-12-08 13:50:19 +0700

    fptbook

---
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DeMoGCS10035.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/home")]
+    public class HomeAdminController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            var author = HttpContext.Session.GetString("user");
+            if (author == null)
+            {
+                return Redirect("/Login");
+            }
+                dynamic? lastAccessInfo;
+                var accessInfoSave = new
+                {
+                    userName = "Demo",
+                    role = "User"
+                };
+                lastAccessInfo = JsonConvert.DeserializeObject(author, accessInfoSave.GetType());
+               if(lastAccessInfo != null && lastAccessInfo?.role != null && lastAccessInfo?.userName != null && lastAccessInfo?.role=="Admin")
+               {
+                ViewData["AdminName"] = lastAccessInfo?.userName;
+                if (lastAccessInfo?.role == "Admin")
+                {
+                    ViewData["Role"] = "Admin";
+                }
+                return View();
+               }
+            return Redirect("/");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using DeMoGCS10035.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace DeMoGCS10035.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string successMessage = TempData["Success"] as string;
+            ViewData["Success"] = successMessage;
+            var author = HttpContext.Session.GetString("user");
+           
+            if (author != null)
+            {
+                dynamic? lastAccessInfo;
+                var accessInfoSave = new
+                {
+                    userName = "Demo",
+                    role = "User"
+                };
+                lastAccessInfo = JsonConvert.DeserializeObject(author, accessInfoSave.GetType());
+                if (lastAccessInfo != null && lastAccessInfo?.role != null && lastAccessInfo?.userName != null && lastAccessInfo?.role == "Admin")
+                {
+                    ViewData["Role"] = lastAccessInfo?.role;
+                }
+                ViewData["Login"] = lastAccessInfo?.userName;
+            }
+            else
+            {
+                ViewData["Login"] = null;
+            }
+            
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
